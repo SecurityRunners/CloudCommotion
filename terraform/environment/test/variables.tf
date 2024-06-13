1ff71da4728b1488b4426ca1a50039e5908d90d2
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2024-06-13 10:51:01 -0500

    adding required variables

---
+++ terraform/environment/test/variables.tf
@@ -0,0 +1,15 @@
+# Global variables
+variable "resource_name" {
+  description = "Convincing bucket name for the organization"
+  type        = string
+}
+
+variable "sensitive_content" {
+  description = "Content of the sensitive file to reach out to an appropriate contact."
+  type        = string
+}
+
+variable "region" {
+  description = "The AWS region to create resources in"
+  type        = string
+}
